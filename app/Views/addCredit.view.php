851354f103dde7e4ecce965886da1eb48fed3a4e
Tim Barmettler <tim.barmettler@hotmail.com>
2020-05-07 13:34:21 +0200

    changes for ftpserver

--- app/Views/addCredit.view.php
+++ app/Views/addCredit.view.php
@@ -26,14 +26,14 @@
             <a href="credits" id="inline-header-button"><button>Abbrechen</button></a>
         </div>
         <?php if (isset($errors) && count($errors) > 0) : ?>
-        <!-- Errors, Teil 1 -->
-        <div class="item">
-            <ul class="text-monospace text-danger">
-                <?php foreach ($errors as $error) : ?>
-                <li><?= $error ?></li>
-                <?php endforeach; ?>
-            </ul>
-        </div>
+            <!-- Errors, Teil 1 -->
+            <div class="item">
+                <ul class="text-monospace text-danger">
+                    <?php foreach ($errors as $error) : ?>
+                        <li><?= $error ?></li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
         <?php endif; ?>
         <div class="item">
             <form action="insertcredit" method="POST">
@@ -41,29 +41,29 @@
                     <legend>Kunde</legend>
                     <label for="name">Name*</label>
                     <input type="text" id="name" name="name" required <?php if (isset($_POST["name"])) {
-                echo "value=\"" . $_POST["name"] . "\"";
-            } ?>><br>
+                                                                            echo "value=\"" . $_POST["name"] . "\"";
+                                                                        } ?>><br>
                     <label for="email">E-Mail*</label>
                     <input type="email" name="email" id="email" required <?php if (isset($_POST["email"])) {
-                echo "value=\"" . $_POST["email"] . "\"";
-            } ?>><br>
+                                                                                echo "value=\"" . $_POST["email"] . "\"";
+                                                                            } ?>><br>
                     <label for="phone">Telefonnummer</label>
                     <input type="text" name="phone" id="phone" <?php if (isset($_POST["phone"])) {
-                echo "value=\"" . $_POST["phone"] . "\"";
-            } ?>>
+                                                                    echo "value=\"" . $_POST["phone"] . "\"";
+                                                                } ?>>
                 </fieldset>
                 <fieldset>
                     <legend>Kredit</legend>
                     <label for="noOfInstallments">Anzahl Raten*</label>
                     <input type="number" name="noOfInstallments" id="noOfInstallments" required <?php if (isset($_POST["noOfInstallments"])) {
-                echo "value=\"" . $_POST["noOfInstallments"] . "\"";
-            } ?>><br>
+                                                                                                    echo "value=\"" . $_POST["noOfInstallments"] . "\"";
+                                                                                                } ?>><br>
                     <label for="creditPackage">Kreditpaket*</label>
                     <select name="creditPackage" id="creditPackage" required>
                         <?php foreach ($creditPackages as $nr => $creditPackage) {
-                    echo "<option value='" . $creditPackage['id'] . "'>" . $creditPackage['name'] . "</option>";
-                }
-                ?>
+                            echo "<option value='" . $creditPackage['id'] . "'>" . $creditPackage['name'] . "</option>";
+                        }
+                        ?>
                     </select>
                 </fieldset>
                 <div class="buttons">
--- core/database.php
+++ core/database.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Stellt eine Verbindung zur Datenbank her und gibt die
  * Datenbankverbindung als PDO zurück.
@@ -8,10 +9,18 @@
 function connectToDatabase()
 {
     try {
-        return new PDO('mysql:host=127.0.0.1;dbname=creditrental', 'root', '', [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-        ]);
+        if ($_SERVER["SERVER_NAME"] == "web.kurse.ict-bz.ch") {
+            return new PDO(
+                "mysql:host=localhost;dbname=kurseictbz_30706",
+                "kurseictbz_30706",
+                "db_307_pw_06"
+            );
+        } else {
+            return new PDO('mysql:host=127.0.0.1;dbname=creditrental', 'root', '', [
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+            ]);
+        }
     } catch (PDOException $e) {
         die('Keine Verbindung zur Datenbank möglich: ' . $e->getMessage());
     }
