046b9b5af600f933abbd539b14a5cc78f8332a05
Tim Barmettler <tim.barmettler@hotmail.com>
2020-05-07 10:40:59 +0200

    sort overview entries

--- app/Controllers/OverviewController.php
+++ app/Controllers/OverviewController.php
@@ -4,4 +4,17 @@ $db = new DbManager();
 $creditrentals = $db->getAll();
 $creditPackages = $db->getCreditPackages();
 
+foreach ($creditrentals as $nr => $credit) {
+    if ($credit["status"] === "1") {
+        unset($creditrentals[$nr]);
+    }
+}
+
+function cmp($a, $b)
+{
+    return strcmp($a["creationDate"], $b["creationDate"]);
+}
+
+usort($creditrentals, "cmp");
+
 require 'app/Views/overview.view.php';
